451377e052219c923d2221150502a20ed67e4ef3
Arman Moaveni <134084996+arimoa@users.noreply.github.com>
2023-10-03 01:38:51 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-var container=document.getElementById("container");
-var feedback=container.getElementsByClassName("feedback");
-var active=document.getElementsByClassName("active");
-var first=document.getElementById("first");
-var container2=document.getElementById("container2");
-var arman2;
-for (var i = 0; i < feedback.length; i++) {
-    feedback[i].addEventListener("click", function() {
-        if (active.length > 0) {
-            active[0].classList.remove("active");
-        }
-        this.classList.add("active");
-        var arman=this.getElementsByClassName("text");
-        arman2=arman[0].innerHTML
-        
-        }
-    )
+const container = document.getElementById("container");
+const feedback = container.getElementsByClassName("feedback");
+const active = document.getElementsByClassName("active");
+const first = document.getElementById("first");
+const container2 = document.getElementById("container2");
+let response;
+for (let i = 0; i < feedback.length; i++) {
+  feedback[i].addEventListener("click", function () {
+    if (active.length > 0) {
+      active[0].classList.remove("active");
+    }
+    this.classList.add("active");
+    let text = this.getElementsByClassName("text");
+    response = text[0].innerHTML;
+  });
 }
-var review=document.getElementById("review");
-review.addEventListener("click",myfun);
+let review = document.getElementById("review");
+review.addEventListener("click", myfun);
 function myfun() {
-    container.style.display="none";
-    container2.style.display="flex";
-    first.innerHTML="Thank you!"
-    document.getElementById("second").innerHTML=arman2;
-    document.getElementById("third").innerHTML="Best luck";
+  container.style.display = "none";
+  container2.style.display = "flex";
+  first.innerHTML = "Thank you!";
+  document.getElementById("second").innerHTML = "Your Feedback: " + response;
+  document.getElementById("third").innerHTML = "Best luck";
 }
